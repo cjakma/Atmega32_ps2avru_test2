bd965cf55207cf92263010d7d720e5d620844080
panhao <panhao4812@users.noreply.github.com>
2017-07-25 14:13:52 +0800

    update

--- Atmega32_ps2avru_test2/main.c
+++ Atmega32_ps2avru_test2/main.c
@@ -7,41 +7,75 @@
 #include "Functions.h"
 
 
-#define ROWS  5
-#define COLS  14
+#define ROWS  15
+#define COLS  8
 #define _delay_after 0x09
 #define _delay_before 0x03
-//lrow B6 F5 F6 F7 F4 B2
-//row D0 D1 D2 D3 D5
-//col F0,F1,E6,C7,C6,B6,D4,B1,B7,B5,B4,D7,D6,B3
+//ledfull D4
+//led D0 D4 D1
+//DAT D3
+//D5--ÎÈÑ¹¶þ¼«¹Ü
+//row A0,A1,A2,A3,A4 A5 A6 A7 C7 C6 C5 C4 C3 C2 C1 C0 D7
+//col B0 B1 B2 B3 B4 B5 B6 B7
 uint8_t i,delayval,FN;
-uint8_t rowPins[ROWS]={5,6,7,8,23};
-uint8_t colPins[COLS]={21,20,24,10,9,15,22,1,4,14,13,12,11,3};
-//                     1  2  3  4  5  6  7 8 9 10 11 12 13 14
+uint8_t rowPins[ROWS]={0,1,2,3,4,5,6,7,23,22,21,20,19,18};
+uint8_t colPins[COLS]={8,9,10,11,12,13,14,15};
+#define ledcount 3
+uint8_t ledPins[ledcount]={24,28,25};
 
 uint8_t hexaKeys0[ROWS][COLS] = {
-	{KEY_ESC,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9,KEY_0,KEY_MINUS,KEY_EQUAL,KEY_BACKSPACE},
-	{KEY_TAB,KEY_Q,KEY_W,KEY_E,KEY_R,KEY_T,KEY_Y,KEY_U,KEY_I,KEY_O,KEY_P,KEY_LEFT_BRACE,KEY_RIGHT_BRACE,KEY_BACKSLASH},
-	{KEY_CAPS_LOCK,KEY_A,KEY_S,KEY_D,KEY_F,KEY_G,KEY_H,KEY_J,KEY_K,KEY_L,KEY_SEMICOLON,KEY_QUOTE,0x00,KEY_ENTER},
-	{KEY_LEFT_SHIFT,0x00,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,0x00, KEY_SLASH,KEY_UP},
-	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,KEY_SPACE,0x00,KEY_SLASH,KEY_LEFT,0x00,KEY_FN,KEY_RIGHT_CTRL,KEY_DOWN, KEY_RIGHT}
+   { KEY_PAUSE,    KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_RCTRL,  KEY_NONE,   KEY_LCTRL,  KEY_F5 },   // ROW0
+   { KEY_Q,        KEY_TAB,    KEY_A,      KEY_ESC,    KEY_Z,      KEY_NONE,   KEY_HASH,   KEY_1 },    // ROW1
+   { KEY_W,        KEY_CAPS,   KEY_S,      KEY_NONE,   KEY_X,      KEY_NONE,   KEY_F1,     KEY_2 },    // ROW2
+   { KEY_E,        KEY_F3,     KEY_D,      KEY_F4,     KEY_C,      KEY_NONE,   KEY_F2,     KEY_3 },    // ROW3
+   { KEY_R,        KEY_T,      KEY_F,      KEY_G,      KEY_V,      KEY_B,      KEY_5,      KEY_4 },    // ROW4
+   { KEY_U,        KEY_Y,      KEY_J,      KEY_H,      KEY_M,      KEY_N,      KEY_6,      KEY_7 },    // ROW5
+   { KEY_I,        KEY_RBR,    KEY_K,      KEY_F6,     KEY_COMMA,  KEY_NONE,   KEY_EQUAL,  KEY_8 },    // ROW6
+   { KEY_O,        KEY_F7,     KEY_L,      KEY_NONE,   KEY_DOT,    KEY_FN,     KEY_F8,     KEY_9 },    // ROW7
+   { KEY_P,        KEY_LBR,    KEY_COLON,  KEY_QUOTE,  KEY_NONE,   KEY_SLASH,  KEY_MINUS,  KEY_0},     // ROW8
+   { KEY_SCRLCK,   KEY_NONE,   KEY_NONE,   KEY_LALT,   KEY_NONE,   KEY_RALT,   KEY_NONE,   KEY_PRNSCR},// ROW9
+   { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE},   // ROW10
+   { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW11
+   { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW12
+   { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW13
+   { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_UP,     KEY_NONE,   KEY_LEFT,   KEY_HOME,   KEY_END }  // ROW14
 };
 uint8_t hexaKeys1[ROWS][COLS] = {
-	{KEY_TILDE,KEY_F1,KEY_F2,KEY_F3,KEY_F4,KEY_F5,KEY_F6,KEY_F7,KEY_F8,KEY_F9,KEY_F10,KEY_F11,KEY_F12, KEY_DELETE},
-	{KEY_TAB,KEYPAD_1,KEYPAD_2,KEYPAD_3,KEYPAD_4,KEYPAD_5,KEYPAD_6,KEYPAD_7,KEYPAD_8,KEYPAD_9,KEYPAD_0,KEYPAD_MINUS,KEYPAD_PLUS,KEY_BACKSLASH},
-	{KEY_CAPS_LOCK, MOUSE_LEFT,MOUSE_MID,MOUSE_RIGHT,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,KEY_ENTER},
-	{KEY_LEFT_SHIFT,0x00,KEY_NUM_LOCK ,KEY_SCROLL_LOCK,KEY_INSERT,KEY_PRINTSCREEN,0x00,0x00,0x00,AUDIO_VOL_DOWN,AUDIO_VOL_UP,0x00,0x00,KEY_UP},
-	{KEY_LEFT_CTRL,KEY_FN,KEY_LEFT_ALT,0x00,0x00,KEY_SPACE,0x00,0x00,KEY_LEFT,0x00,KEY_FN,KEY_RIGHT_CTRL,KEY_DOWN,KEY_RIGHT}
+	 { KEY_NUMLOCK,  KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_RCTRL,  KEY_NONE,   KEY_LCTRL,  KEY_F5 },   // ROW0
+	 { KEY_Q,        KEY_TAB,    KEY_A,      KEY_ESC,    KEY_Z,      KEY_NONE,   KEY_HASH,   KEY_1 },    // ROW1
+	 { KEY_W,        KEY_CAPS,   KEY_S,      KEY_NONE,   KEY_X,      KEY_NONE,   KEY_F1,     KEY_2 },    // ROW2
+	 { KEY_E,        KEY_F3,     KEY_D,      KEY_F4,     KEY_C,      KEY_NONE,   KEY_F2,     KEY_3 },    // ROW3
+	 { KEY_R,        KEY_T,      KEY_F,      KEY_G,      KEY_V,      KEY_B,      KEY_5,      KEY_4 },    // ROW4
+	 { KEY_KP_4,     KEY_Y,      KEY_KP_1,   KEY_H,      KEY_KP_0,   KEY_N,      KEY_6,      KEY_KP_7 }, // ROW5
+	 { KEY_KP_5,     KEY_RBR,    KEY_KP_2,   KEY_F6,     KEY_KP_0,   KEY_NONE,   KEY_EQUAL,  KEY_KP_8 }, // ROW6
+	 { KEY_KP_6,     KEY_F7,     KEY_KP_3,   KEY_NONE,   KEY_KP_DOT, KEY_FN,     KEY_F8,     KEY_KP_9 }, // ROW7
+	 { KEY_KP_AST,   KEY_LBR,    KEY_KP_MINUS,KEY_QUOTE, KEY_NONE,   KEY_KP_PLUS,KEY_MINUS,  KEY_KP_SLASH},  // ROW8
+	 { KEY_LED,      KEY_NONE,   KEY_NONE,   KEY_LALT,   KEY_NONE,   KEY_RALT,   KEY_NONE,   KEY_PRNSCR},// ROW9
+	 { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE},   // ROW10
+	 { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW11
+	 { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW12
+	 { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE,   KEY_NONE}, // ROW13
+	 { KEY_NONE,     KEY_NONE,   KEY_NONE,   KEY_UP,     KEY_NONE,   KEY_LEFT,   KEY_HOME,   KEY_END }  // ROW14
 };
 //keymask_bits:7-press 654-hexatype0 3-press 210-hexatype1
 //type: 1-key 2-modifykey 3-mousekey 4-systemkey 5-consumerkey 6-FN 7-consumerkeyAL,8-consumerkeyAC
 
-uint8_t keymask[ROWS][COLS] = {
-	{0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11},
-	{0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11},
-	{0x11,0x13,0x13,0x13,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x00,0x11},
-	{0x22,0x00,0x11,0x11,0x11,0x11,0x10,0x10,0x10,0x15,0x15,0x00,0x10,0x11},
-	{0x22,0x66,0x22,0x00,0x00,0x11,0x00,0x10,0x11,0x00,0x66,0x22,0x11,0x11}
+uint8_t keymask[ROWS][COLS]={
+	{0x11,0x00,0x00,0x00,0x22,0x00,0x22,0x11},//ROW0
+	{0x11,0x11,0x11,0x11,0x11,0x00,0x11,0x11},//ROW1
+	{0x11,0x11,0x11,0x00,0x11,0x00,0x11,0x11},//ROW2
+	{0x11,0x11,0x11,0x11,0x11,0x00,0x11,0x11},//ROW3
+	{0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11},//ROW4
+	{0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11},
+	{0x11,0x11,0x11,0x11,0x11,0x00,0x11,0x11},
+	{0x11,0x11,0x11,0x00,0x11,0x11,0x11,0x11},
+	{0x11,0x11,0x11,0x11,0x00,0x11,0x11,0x11},
+	{0x11,0x00,0x00,0x22,0x00,0x22,0x00,0x11},
+	{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+	{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+	{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+	{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+	{0x00,0x00,0x00,0x11,0x00,0x11,0x11,0x11},
 };
 
 void init_cols(){
@@ -57,8 +91,7 @@ void init_rows(){
 	}
 }
 /////////////////////////////////////////////////////////////////////
-#define ledcount 2
-uint8_t ledPins[ledcount]={18,2};
+
 void Open_LED(){
 	for ( i=0; i<ledcount; i++){
 		digitalWrite(ledPins[i],LOW);
